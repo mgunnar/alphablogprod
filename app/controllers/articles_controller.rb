9df5dc7ab0e637494ca4771db5bb40c144e8b2de
Matheus <devmatheus@gmail.com>
2022-10-20 13:52:30 -0300

    Refactoring.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,9 @@
 class ArticlesController < ApplicationController
+
+  before_action :set_article, only:[:show, :edit, :update, :destroy]
+
   def show
-    @article = Article.find(params[:id])
+    
   end
 
   def index
@@ -12,35 +15,43 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     if @article.save
-        flash[:notice] = "Article was created successfully."
-    redirect_to @article
+      flash[:notice] = 'Article was created successfully.'
+      redirect_to @article
     else
-        render 'new'
+      render 'new'
     end
-    end
+  end
 
-    def edit
-        @article = Article.find(params[:id])
-    end
+  def edit
+    
+  end
 
-    def destroy
-      @article = Article.find(params[:id])
-      @article.destroy
-      redirect_to articles_path
+  def destroy
+    
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  def update
+    
+    if @article.update(article_params)
+      flash[:notice] = 'Article was updated successfully.'
+      redirect_to @article
+    else
+      render 'edit'
     end
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
   
-    
-    def update
-        @article = Article.find(params[:id])
-        if @article.update(params.require(:article).permit(:title, :description))
-            flash[:notice] = "Article was updated successfully."
-            redirect_to @article
-        else
-            render 'edit'
-        end
-    end
-        
-    
 end
